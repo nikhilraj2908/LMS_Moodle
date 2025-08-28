91ef48a55258470ff3074c1e9d33cfec80d9639c
Nikhil Ahirwar <nikhilraj2908@gmail.com>
2025-08-28 12:37:21 +0530

    corrected the side drawer problem

--- theme/academi/javascript/drawerfix.js
+++ theme/academi/javascript/drawerfix.js
@@ -1,15 +1,13 @@
 (function () {
-  const page = document.getElementById('page');
   function clearBodyComp() {
     document.body.style.paddingRight = '';
     document.documentElement.style.paddingRight = '';
-    if (page && !document.body.classList.contains('drawer-open-right')) {
-      page.classList.remove('show-drawer-right');
-    }
   }
+
   new MutationObserver(() => {
     if (!document.body.classList.contains('drawer-open-right')) clearBodyComp();
   }).observe(document.body, { attributes: true, attributeFilter: ['class','style'] });
+
   window.addEventListener('pageshow', clearBodyComp);
   window.addEventListener('resize', clearBodyComp);
   document.addEventListener('click', (e) => {
